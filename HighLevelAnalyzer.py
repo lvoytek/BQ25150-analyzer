6bd40fe19fe4326002f7fe9ca5ab5e88014f0503
Lena Voytek <lena.voytek@garmin.com>
2021-07-26 10:39:34 -0700

    Added register definitions
    Change-Id: I957fbba5bec8100aa6245385c574519604b09ca7

--- HighLevelAnalyzer.py
+++ HighLevelAnalyzer.py
@@ -3,6 +3,67 @@
 
 from saleae.analyzers import HighLevelAnalyzer, AnalyzerFrame, StringSetting, NumberSetting, ChoicesSetting
 
+import struct
+
+DATA_COMMANDS = {
+
+}
+
+REGISTERS = {
+  0x00 : "Charger Status 0",
+  0x01 : "Charger Status 1",
+  0x02 : "ADC Status",
+  0x03 : "Charger Flags 0",
+  0x04 : "Charger Flags 1",
+  0x05 : "ADC Flags",
+  0x06 : "Timer Flags",
+  0x07 : "Interrupt Mask 0",
+  0x08 : "Interrupt Mask 1",
+  0x09 : "Interrupt Mast 2",
+  0x0a : "Interrupt Mask 3",
+  0x12 : "Battery Voltage Control",
+  0x13 : "Fast Charge Current Control",
+  0x14 : "Pre-Charge Current Control",
+  0x15 : "Termination Current Control",
+  0x16 : "Battery UVLO and Current Limit Control",
+  0x17 : "Charger Control 0",
+  0x18 : "Charger Control 1",
+  0x19 : "Input Current Limit Control",
+  0x1d : "LDO Control",
+  0x30 : "MR Control",
+  0x35 : "IC Control 0",
+  0x36 : "IC Control 1",
+  0x37 : "IC Control 2",
+  0x40 : "ADC Control 0",
+  0x41 : "ADC Control 1",
+  0x42 : "VBatt MSB",
+  0x43 : "VBatt LSB",
+  0x44 : "TS MSB",
+  0x45 : "TS LSB",
+  0x46 : "Fast Charge Current MSB",
+  0x47 : "Fast Charge Current LSB",
+  0x48 : "ADC In MSB",
+  0x49 : "ADC In LSB",
+  0x4a : "Vin MSB",
+  0x4b : "Vin LSB",
+  0x4c : "VpMid MSB",
+  0x4d : "VpMid LSB",
+  0x4e : "Iin MSB",
+  0x4f : "Iin LSB",
+  0x52 : "Comparator 1 Threshold MSB",
+  0x53 : "Comparator 1 Threshold LSB",
+  0x54 : "Comparator 2 Threshold MSB",
+  0x55 : "Comparator 2 Threshold LSB",
+  0x56 : "Comparator 3 Threshold MSB",
+  0x57 : "Comparator 3 Threshold LSB",
+  0x58 : "ADC Channel Enable",
+  0x61 : "Fast Charge Control",
+  0x62 : "Cold Threshold",
+  0x63 : "Cool Threshold",
+  0x64 : "Warm Threshold",
+  0x65 : "Hot Threshold",
+  0x6f : "Device ID"
+}
 
 # High level analyzers must subclass the HighLevelAnalyzer class.
 class Hla(HighLevelAnalyzer):
